57ec008443a5a3389ac5f7173574f8facf01eb9e
Iskren Ivanov <iskreniivanov@gmail.com>
2018-10-13 23:20:42 +0300

    Initial Website Finished

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNavItems = document.querySelectorAll('.nav-item');
+
+//Set Initial State of MENU
+let showMenu = true;
+
+menuBtn.addEventListener('click', toggleMenu);
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    menuNavItems.forEach(item => item.classList.add('show'));
+
+    //Reset Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    menuNavItems.forEach(item => item.classList.remove('show'));
+
+    //Reset Menu State
+    showMenu = false;
+  }
+}
